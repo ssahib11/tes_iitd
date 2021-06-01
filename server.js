be5b707bd4a2bca71f069fd1eadcc4eed3b14728
Sahib <sahibsaify586@gmail.com>
2021-06-02 02:17:56 +0530

    add dotenv and set environment variables in .env

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
