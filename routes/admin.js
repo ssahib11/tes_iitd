00e8e253bd2546a6f10622e70145e7f60dccd0c0
Sahib <sahibsaify586@gmail.com>
2021-06-02 02:18:46 +0530

    moved routes.js to routes dir and created routes/admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,22 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const db = require("../models");
+
+// db.sequelize.sync();
 
 router.get('/', (req, res) => {
     res.send("Hello Admin, under construction");
+});
+
+router.get('/events', async (req, res) => {
+    return db.Events.findAll()
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => 
+            res.status(500).send({
+                message: err.message || "Could not process query"
+            }));
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
-const routes = require('express').Router();
+const express = require('express');
+const routes = express.Router();
+const db = require("../models");
 
 const posts = [
     {
@@ -36,6 +38,11 @@ routes.get('/', (req, res) => {
 
 routes.get('/first', (req, res) => {
     res.send('Hello From Index');
+});
+
+routes.get('/events', async (req, res) => {
+    const events = await db.Events.findAll(); 
+    res.send(events);
 });
 
 module.exports = routes;
