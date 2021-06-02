d9af8e4e8df4816b47fcddcdf6e489a07a570936
Sahib Saify <sahibsaify586@gmail.com>
2021-06-02 22:14:45 +0530

    add morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
 // const routes = require('./routes');
 
 const indexRouter = require('./routes/index');
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
