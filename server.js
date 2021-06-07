20c54180b82d8099a56eaf4cdd320c3468ccad93
Sahib Saify <sahibsaify586@gmail.com>
2021-06-07 14:59:03 +0530

    add login and authentication protocol for admin portal

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 // const routes = require('./routes');
+
+require('./config/passport')(passport);
 
 const indexRouter = require('./routes/index');
 const adminRouter = require('./routes/admin');
@@ -17,6 +21,19 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    rolling: true,
+    saveUninitialized: false, 
+    cookie: {
+        maxAge: 2 * 3600000
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
