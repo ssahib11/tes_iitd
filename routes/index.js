1f5bea68612fbe6def78f82e8901b46868c7abd6
Sahib Saify <sahibsaify586@gmail.com>
2021-06-12 04:43:18 +0530

    add dummy controllers for /events, /team, /contact

--- routes/index.js
+++ routes/index.js
@@ -36,13 +36,64 @@ routes.get('/', (req, res) => {
     res.render('index');
 });
 
-routes.get('/first', (req, res) => {
-    res.send('Hello From Index');
+routes.get('/events', async (req, res) => {
+    const eventList = [
+        {
+            title: "Event 1",
+            date: "2021-05-15",
+            time: "12:03",
+            description: "Description 1",
+            image_path: "/images/events/image1.jpg"
+        },
+        {
+            title: "Event 2",
+            date: "2021-05-15",
+            time: "12:03",
+            description: "Description 2",
+            image_path: "/images/events/image1.jpg"
+        },
+        {
+            title: "Event 3",
+            date: "2021-05-15",
+            time: "12:03",
+            description: "Description 3",
+            image_path: "/images/events/image1.jpg"
+        }
+
+    ];
+    res.render('events', { eventList: events });
 });
 
-routes.get('/events', async (req, res) => {
-    const events = await db.Events.findAll(); 
-    res.send(events);
+routes.get('/blog', async (req, res) => {
+    res.render('blog');
+});
+
+routes.get('/team', async (req, res) => {
+    const hod = {
+        name: "HOD 1",
+        designation: "HOD",
+    };
+    const genSec = {
+        name: "GSEC 1",
+        designation: "GSEC",
+    };
+    const coord = {
+        name: "COORD 1",
+        designation: "COORD",
+    };
+    const exes = {
+        name: "EXES 1",
+        designation: "EXES",
+    };
+    res.render('tesTeam', { hod: hod, generalSecretary: genSec, coordinators: coord, executives: exes });
+});
+
+routes.get('/contact', async (req, res) => {
+    const genSec = {
+        name: "GSEC 1",
+        designation: "GSEC",
+    };
+    res.render('contactUs', { generalSecretary: genSec });
 });
 
 module.exports = routes;
