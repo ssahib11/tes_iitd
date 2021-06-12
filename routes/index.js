3162102ba73ac564ffaeb67c03937eb5d0b873c8
Praveen Gupta <prvengpta@gmail.com>
2021-06-12 14:12:16 +0530

    Bugs Resolved + Changed the structure of Object received from db

--- routes/index.js
+++ routes/index.js
@@ -43,25 +43,26 @@ routes.get('/events', async (req, res) => {
             date: "2021-05-15",
             time: "12:03",
             description: "Description 1",
-            image_path: "/images/events/image1.jpg"
+            image: "/images/events/image1.jpg"
         },
         {
             title: "Event 2",
             date: "2021-05-15",
             time: "12:03",
             description: "Description 2",
-            image_path: "/images/events/image1.jpg"
+            image: "/images/events/image1.jpg"
         },
         {
             title: "Event 3",
             date: "2021-05-15",
             time: "12:03",
             description: "Description 3",
-            image_path: "/images/events/image1.jpg"
+            image: "/images/events/image1.jpg"
         }
 
     ];
-    res.render('events', { eventList: events });
+
+    res.render('events', { eventList: eventList });
 });
 
 routes.get('/blog', async (req, res) => {
@@ -69,30 +70,56 @@ routes.get('/blog', async (req, res) => {
 });
 
 routes.get('/team', async (req, res) => {
-    const hod = {
-        name: "HOD 1",
-        designation: "HOD",
-    };
-    const genSec = {
-        name: "GSEC 1",
-        designation: "GSEC",
-    };
-    const coord = {
-        name: "COORD 1",
-        designation: "COORD",
-    };
-    const exes = {
-        name: "EXES 1",
-        designation: "EXES",
-    };
+    const hod = [
+        {
+            name: "HOD 1",
+            designation: "HOD",
+        },
+    ];
+    const genSec = [
+        {
+            name: "GSEC 1",
+            designation: "GSEC",
+        },
+    ];
+    const coord = [
+        {
+            name: "COORD 1",
+            designation: "COORD",        
+        },
+        {
+            name: "COORD 1",
+            designation: "COORD",        
+        },
+        {
+            name: "COORD 1",
+            designation: "COORD",        
+        },
+    ];
+    const exes = [
+        {
+            name: "EXES 1",
+            designation: "EXES",
+        },
+        {
+            name: "EXES 1",
+            designation: "EXES",
+        },
+        {
+            name: "EXES 1",
+            designation: "EXES",
+        },
+    ];
     res.render('tesTeam', { hod: hod, generalSecretary: genSec, coordinators: coord, executives: exes });
 });
 
 routes.get('/contact', async (req, res) => {
-    const genSec = {
-        name: "GSEC 1",
-        designation: "GSEC",
-    };
+    const genSec = [
+        {
+            name: "GSEC 1",
+            designation: "GSEC",
+        },
+    ];
     res.render('contactUs', { generalSecretary: genSec });
 });
 
